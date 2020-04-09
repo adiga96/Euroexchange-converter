b7b1400db04fb8121157c3c6cc334f5c17b85957
wilsonjosiu <60586242+wilsonjosiu@users.noreply.github.com>
2020-04-09 03:22:46 -0400

    Add files via upload

--- euro_currency_converter.py
+++ euro_currency_converter.py
@@ -28,14 +28,13 @@ def desiredDate(dates): #entered value must be a string
 # Next, we will check the currency exchange rate based on 1 euro
 #This means that we will obtain all the currencies values equal to 1 euro 
     conn.request("GET", "/currency/convert", headers=headers)
-
     resp = conn.getresponse()
     data2 = resp.read().decode("utf-8")
     convert = json.loads(data2)
-  
+
+#change the data to a dataframe  
     currency_df= pd.DataFrame(currency)
     print(currency_df)
-
 
     print("Base amount Value: ",convert['amount'])
 
@@ -43,29 +42,30 @@ def desiredDate(dates): #entered value must be a string
 
     print("Currency Base Code: ",convert['base_currency_code'])
 
+# we put the current date to have a better visualization of the output
     today = date.today()
     print("Today's date:", today)
 
     print('\n')
     print('Today, the exchange currency values are:\n')
 
+#change the data to a dataframe
     convert_df= pd.DataFrame(convert['rates'])
     convert_df = convert_df.drop('rate_for_amount')
     print(convert_df)
 
-
     print('\n')
     print(dates, "The requested date exchange currency values are:\n")
 
-
+# prepare the dynamic code so we may choose which date we want to see the currency
     Requesteddate ="/currency/historical/" + dates + ""
 
-
     conn.request("GET", Requesteddate, headers=headers)
-
     resp = conn.getresponse()
     data = resp.read().decode("utf-8")
     historical = json.loads(data)
+    
+#change the data to a dataframe
     historical_df= pd.DataFrame(historical['rates'])
     historical_df = historical_df.drop('rate_for_amount')
     print(historical_df)
